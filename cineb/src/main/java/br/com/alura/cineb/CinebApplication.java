146ccab5ec96d2a84813fe4e20d574b94d860bac
~belletb <~belletbassi@gmail.com>
2026-06-17 15:56:14 -0400

    Adiciona dados de episodios e temporadas

--- cineb/src/main/java/br/com/alura/cineb/CinebApplication.java
+++ cineb/src/main/java/br/com/alura/cineb/CinebApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.cineb;
 
+import br.com.alura.cineb.model.DadosEpisodio;
 import br.com.alura.cineb.model.DadosSerie;
+import br.com.alura.cineb.model.DadosTemporadas;
 import br.com.alura.cineb.service.ConsumoAPI;
 import br.com.alura.cineb.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class CinebApplication implements CommandLineRunner {
@@ -21,6 +26,19 @@ public class CinebApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporadas> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			DadosTemporadas dadosTemporada = conversor.obterDados(json, DadosTemporadas.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
 
